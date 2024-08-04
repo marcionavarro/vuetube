ab33d01ce86cd6411b444ce38f492137dfa2f8da
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-08-04 17:59:16 -0300

    AAE IdeaPro[Portfolio UI Design vue 3] - Menu, Head

---
+++ portfolio/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn>
+      <v-icon color="yellow" lef class="mr-2">fas fa-signature</v-icon>MarcioNavarro
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" class="text-yellow">Home</v-btn>
+    <v-btn text @click="scroll('about')">Sobre</v-btn>
+    <v-btn text @click="scroll('portfolio')">Portfólio</v-btn>
+    <v-btn text @click="scroll('services')">Serviços</v-btn>
+    <v-btn text @click="scroll('page')">Página</v-btn>
+    <v-btn text @click="scroll('blob')">Blog</v-btn>
+    <v-btn text @click="scroll('contact')">Contato</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName)
+      element.scrollIntoView({behavior: 'smooth'})
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,18 +1,79 @@
 <template>
-  <HelloWorld />
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+      <div class="head">
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative" class="mt-16">
+              <h1 class="text-grey">Olá,</h1>
+              <h1 class="text-grey">Eu sou Marcio Navarro</h1>
+              <span class="text-gry">Web Designer & Developer</span><br />
+              <v-btn tile dark class="text-yellow mt-8" variant="outlined">Contate-me</v-btn>
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div style="position: absolute; z-index: 9999; bottom: -0; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center;">
+              <v-icon>fa fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+          <v-col cols="5">
+            <div style="position: relative; z-index: 9999;" class="mt-16">
+              <v-img src="i1.png" contain max-height="300"></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import NavBar from '../components/NavBar.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    NavBar,
   },
 });
 </script>
+
+<style scoped>
+  .v-container{
+    padding: 16px 0;
+  }
+  .head{
+    position: relative;
+    text-align: center;
+    padding: 12px;
+    margin-bottom: 6px;
+    height: 400px;
+    width: 100%;
+    color: white;
+  }
+  .head::before{
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 50%;
+    background: black;
+    transform: skew(0deg, 6deg);
+  }
+  .head::after{
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 50%;
+    background: black;
+    transform: skew(0deg, -6deg);
+  }
+</style>
